2b3e3b034757bb80bfeea0319cfdbbd2dce94efb
Seu Nome ou Instituto Kamaleon <guisinosini@gmail.com>
2026-06-25 20:38:55 -0300

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,17 @@ export default function RootLayout({
             {children}
           </main>
         </ReservationProvider>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/sw.js');
+                });
+              }
+            `,
+          }}
+        />
       </body>
     </html>
   );
